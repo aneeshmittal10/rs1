2bed3502c02729a50f1cc1ea83dc6286877d1102
aneeshmittal10 <55001630+aneeshmittal10@users.noreply.github.com>
2022-06-16 18:35:26 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'RickyPedia Space',
+  domain: 'rickypedia.space',
+  author: 'Aneesh Mittal',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'RickyPedia Space',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'rickypedia.space',
+  github: 'rickypedia10',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -62,3 +61,4 @@ export default siteConfig({
   //   }
   // ]
 })
+
